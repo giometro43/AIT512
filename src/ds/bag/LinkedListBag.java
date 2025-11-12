2bfcb8cba45275de01c8d3abd86d23b506d8d32a
giometro43 <giocdbz@gmail.com>
2025-11-12 00:16:40 -0800

    Created the LinkedListBag, deleted the test cases by creating a general bag method and making the classes

--- src/ds/bag/LinkedListBag.java
+++ src/ds/bag/LinkedListBag.java
@@ -31,22 +31,166 @@ import java.util.Iterator;
  */
 public class LinkedListBag<Item> implements Bag<Item> {
 
+	/**
+	 * A node in the list
+	 */
+	private class Node {
+		
+		/**
+		 * the next node, following after this node, null if none
+		 */
+		
+		
+		Node nextNode;
+		
+		// the item stored at this node
+		Item item;
+	}
+	
+	
+	/**
+	 * the start node in the lsit of items
+	 */
+	public Node startNode;
+	
+	/**
+	 *  the end node in the list of items 
+	 */
+	
+	public Node endNode;
+	
+	
+	
+	/**
+	 * the number of elements in the list
+	 */
+	private int numberOfElements; // create a field for the elements 
+	
+	
+	
+	
+	
+	/**
+	 * creates an empty list of items
+	 */
+	public LinkedListBag() {
+		startNode =null; //the default when an object has no value. (NULL)
+		endNode =null;
+		numberOfElements =0;
+	
+	}
+	
+	
+	
 	@Override
 	public Iterator<Item> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+			
+		return new Iterator<Item>() {
+
+			
+			private Node currentNode=startNode;
+			
+			@Override
+			public boolean hasNext() {
+				
+				return currentNode!=null;
+			}
+
+			@Override
+			public Item next() {
+				Item item = currentNode.item;
+				currentNode=currentNode.nextNode;
+				return item;
+			}
+			
+		};
 	}
 
 	@Override
 	public void add(Item item) {
 		// TODO Auto-generated method stub
+
+	/**
+	 * We will create a new node, for each item we want to add
+	 * 
+	 * an item node that in which we want to store the new item
+	 * 
+	 * so we will put in the nextNode for the item, since there will be nothing after it (NULL)
+	 * 
+	 * also we need to put the item field of the itemNode. the item that we want to add
+	 * 
+	 * BREAKDOWN:
+	 * create new node
+	 * create item node field
+	 * then increment the number of elements
+	 * 
+	 *  
+	 * 
+	 * 
+	 */
+		Node itemNode = new Node();
+		itemNode.nextNode = null;
+		itemNode.item = item;
+		numberOfElements++;
+		
+		
+		/*add this node at the end of the bag, not the beginning.
+		 * 
+		 * 
+		 * wa want to add this node at the end of the bag, two cases, if the list is empty we need to intialize the list
+		 * this will be the first element that we put in the list
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * so we must check if the list is empty ( if the start node is null), if that is the case then the item node
+		 * be the start and the end of the list.
+		 * 
+		 * 
+		 * if it is Not empty then we only need to add it as the last element in the list
+		 * we just look at the previous end node, now the next node is the itemNode we created
+		 * 
+		 * 
+		 * 
+		 * 
+		 * BREAKDOWN
+		 * think of the linked list being like a horizontal bag
+		 * and we cannot leave things on the left of the bag since it starts there
+		 * so we move it to the very right even if its the first item. so that we can 
+		 * make room for the other items if they come. 
+		 * 
+		 * IF, the the start node is null, that means the bag is empty.
+		 * since the bag is empty there is nothing in the start or end node values
+		 * we add the itemNode into start AND end node.
+		 * 
+		 * 
+		 * ELSE: if the start node has a value, we know that same value is in the end node,
+		 * 
+		 * if its not empty, then we need to add the item as the last element of the list
+		 * 
+		 * 
+		*/
+		if(startNode ==null) {
+			startNode=itemNode;
+			endNode=itemNode;
+		}else {
+			endNode.nextNode = itemNode; //now we linked this previous list to this item node
+			
+			// the new end node will bne the item node
+			endNode = itemNode;
+		}
+		
+		
+		
 		
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+	//
+		return startNode == null;
 	}
 
 	@Override
@@ -58,8 +202,13 @@ public class LinkedListBag<Item> implements Bag<Item> {
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return numberOfElements;
 	}
+	
+	public String toString() {
+		return toString ("[", "]" , " " );
+	}
+	
 	
 
 }
--- src/ds/bag/TestBags.java
+++ src/ds/bag/TestBags.java
@@ -6,44 +6,15 @@ package ds.bag;
  * @author gcastillo
  */
 public class TestBags {
-	/**
-	 * Basic test of a fixed capacity bag
-	 */
-	public static void testFixedCapacityBag() {
+	
+public static void testBag(Bag<String> bag) {
 		
 		System.out.println("Create an empty bag with capacity 5.");
 		
-		FixedCapacityBag<String> bag = new FixedCapacityBag<>(5); // creating a fixed capacity bag
-		// capacity of 5
-		System.out.println("Bag is a singleton:" + bag.isSingleton());// TASK 2C
-		System.out.println("Bag: " + bag.toString());//print the bag 
-		System.out.println("add the item: SING: the movie tickets");
-		bag.add("movie ticket(x2)");
-		System.out.println("Bag is a singleton: " + bag.isSingleton());// TASK 2C
-		System.out.println("Bag: " + bag.toString());//print the bag
-		System.out.println("add the items: hair tie, gray sweater, roscoes chicken, signal hill momento");
-		// nostalgia from my past haha
-		bag.add("hair tie");
-		bag.add("gray sweater");
-		bag.add("roscoes chicken");
-		bag.add("signal hill momento");
-		System.out.println("Bag: " + bag.toString());
-		System.out.println("Bag is a singleton:" + bag.isSingleton());// TASK 2C
-		System.out.println("How many items start with b?");
-		int count = 0;
-		for (String item: bag) {
-			if (item.charAt(0)=='b') count++; //using the char at method to see the
-		}
-		System.out.println("There are "+count+" items that start with B.");
 		
-	}
-	
-	
-public static void testDynamicCapacityBag() {
 		
-		System.out.println("Create an empty bag with capacity 5.");
-		
-		Bag <String> bag = new DynamicCapacityBag<>(); // creating a fixed capacity bag before of 5, but now it uses the default from the DCB class.
+		//can get rid of line below since its now a generic bag creator
+		//Bag <String> bag = new DynamicCapacityBag<>(); // creating a fixed capacity bag before of 5, but now it uses the default from the DCB class.
 		// capacity of 10 predefined
 		System.out.println("Bag is a singleton:" + bag.isSingleton());// TASK 2C
 		System.out.println("Bag: " + bag.toString());//print the bag 
@@ -58,12 +29,16 @@ public static void testDynamicCapacityBag() {
 		bag.add("roscoes chicken");
 		bag.add("signal hill momento");
 		System.out.println("Bag: " + bag.toString());
-
+		try { 
+			
+		
 		System.out.println("add the sixth item. MONEYY");
 
 		bag.add("961,600,000,000 Dollars");//new sixth item, DoD budget
 		System.out.println("Bag: " + bag.toString());
-
+		}catch (Exception e) { 
+			System.out.println("Error trying to add the sixth item: " + e.getMessage());
+		}
 		
 		System.out.println("Bag is a singleton:" + bag.isSingleton());// TASK 2C
 		System.out.println("How many items start with b?");
@@ -97,9 +72,15 @@ public static void testDynamicCapacityBag() {
 	 */ 
 	public static void main(String[] args) {
 		System.out.println("Dynamic Capacity Bag - Task 1 - by Giovanni Castillo");
-		//testFixedCapacityBag();
-
-		testDynamicCapacityBag();
+		
+		Bag<String> b1 = new FixedCapacityBag<>(5);
+		Bag<String> b2 = new DynamicCapacityBag<>(5);
+		Bag<String> b3 = new LinkedListBag<>();
+		
+		testBag(b1);
+		
+		testBag(b2);
+		testBag(b3);
 	}
 
 }
