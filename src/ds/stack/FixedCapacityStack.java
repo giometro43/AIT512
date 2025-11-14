bd8bc9cd9ecb9464a827de24ed939fa4d73ccb17
giometro43 <giocdbz@gmail.com>
2025-11-13 17:57:47 -0800

    stuff

--- src/ds/stack/FixedCapacityStack.java
+++ src/ds/stack/FixedCapacityStack.java
@@ -2,9 +2,9 @@ package ds.stack;
 
 public class FixedCapacityStack {
 	/**
-	 * Module:
-	 * Assignment: 
-	 * @author gcastillo
+	 * Module: 1 - Bags, Stacks and Queues
+	 * Assignment: A21-PA1 Fixed Capacity Stack
+	 * @author gcastil9@gmu.edu
 	 */
 }
 
