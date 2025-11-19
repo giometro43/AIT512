cc99e1f24ba020cc1ffde8221a763ead83b0ee3f
giometro43 <giocdbz@gmail.com>
2025-11-18 18:03:25 -0800

    Implement DynamicCapacityQueue with Javadoc and inline comments; update TestQueues for dynamic testing

--- src/ds/queue/DynamicCapacityQueue.java
+++ src/ds/queue/DynamicCapacityQueue.java
@@ -1,9 +1,25 @@
 package ds.queue;
-
+/*
+ *Tasks:
+Perform these tasks based on these learning materials, do not include elements from other videos. 
+The program will be modified in future assignments. However, the TestQueue must continue the previous homework file.  
+Task 1: Implement the class DynamicCapacityQueue.
+In the package (ds.queue) define the class implementation DynamicCapacityQueue based on the
+dynamic capacity bag and stack implementation, and taking in account the modificattions from the fixed capacity queue.
+You must add javadoc and inline comments to express the functionality in your own words.
+Modify the class TestQueues in a similar way with TestStacks, by creating a generic method testQueue 
+that will receive an empty queue, a name for the queue, an array of items that will be enqueued, 
+a number of elements to be dequeued, another array of items to be enqueue and another numnber of elements to be dequeue. 
+Call the new test twice from the main method for a dynamic capacitty queue of strings and again for integers.
+Modify TestQueues class to print at the beginning of the test the message (where you replace my name with your name):
+Dynamic Capacity Queue - Task 1 - by Mihai Boicu
+Test and debug your program
+EXECUTION PDF: increase the size of the output area (maximize), add two screenshots of the execution
+showing the beggining and the end of the output.*/
 public class DynamicCapacityQueue {
 	/**
-	 * Module:
-	 * Assignment: 
+	 * Module: 2 - Bags, Stacks, and Queues
+	 * Assignment:  A22-PA2 Dynamic Capacity Queue
 	 * @author gcastillo
 	 */
 }
